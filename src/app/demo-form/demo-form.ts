9b859081d6161b28f989fd6567f8f086a9cb9e47
martin <martinvondracek@gmail.com>
2016-01-13 11:47:00 +0100

    new service for creating customer

--- src/app/demo-form/demo-form.ts
+++ src/app/demo-form/demo-form.ts
@@ -2,11 +2,14 @@ import { Component } from 'angular2/core';
 import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators,
   AbstractControl, Control} from 'angular2/common';
 
+import {CustomerService} from './../services/customer.service';
+
 @Component({
   selector: 'demo-form',
   styles: [ require('./demo-form.css') ],
   template: require('./demo-form.html'),
-  directives: [FORM_DIRECTIVES]
+  directives: [FORM_DIRECTIVES],
+  providers: [CustomerService]
 })
 
 export class DemoForm {
@@ -19,7 +22,7 @@ export class DemoForm {
     }
   }
 
-  constructor(fb: FormBuilder) {
+  constructor(fb: FormBuilder, private customerService: CustomerService) {
     this.myForm = fb.group({
       firstName: ['Miro', Validators.required],
       lastName: ['Bulo', Validators.required],
@@ -32,19 +35,19 @@ export class DemoForm {
 
     this.myForm.controls['firstName'].valueChanges.subscribe(
       (value: string) => {
-        console.log('name value changed: ', value);
+        //console.log('name value changed: ', value);
       }
     );
 
     this.myForm.valueChanges.subscribe(
       (value: string) => {
-        console.log('form value changed: ', value);
+        //console.log('form value changed: ', value);
       }
     );
   }
 
   onSubmit(model) {
-    console.log('submitted: ' + JSON.stringify(model));
+    this.customerService.createCustomer(model);
   }
 
 }
